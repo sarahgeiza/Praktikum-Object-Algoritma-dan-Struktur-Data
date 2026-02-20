a928ca859bc379e05632c0be2994dd2d4259fb61
sarahgeiza <sarahgeizask@gmail.com>
2026-02-20 09:05:37 +0700

    Percobaan 3

--- MahasiswaMain25.java
+++ MahasiswaMain25.java
@@ -7,8 +7,12 @@ public class MahasiswaMain25 {
         mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
-        mhs1.ubahKelas("SI 2K");
+        mhs1.ubahKelas("TI 1D");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa25 mhs2 = new Mahasiswa25("Anissa Nabila","2141720160", 3.25,"TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
